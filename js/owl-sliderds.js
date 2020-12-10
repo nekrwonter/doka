b6c78a00b102fe25eb03287337f863d8df75cc30
Deus <nekrwonter@gmail.com>
2020-12-11 01:41:48 +0200

    Logos slider

--- js/owl-sliderds.js
+++ js/owl-sliderds.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
         loop:true,
         dots:true,
         dotsClass: "dots",
-        dotsContainer: ".dots-doka",
+        dotsContainer: ".dots-complect",
         responsiveClass:true,
         responsive:{
             1200 : {
@@ -33,6 +33,29 @@ $(document).ready(function(){
             // breakpoint from 480 up
             1000 : {
                 items:3
+            },
+            // breakpoint from 768 up
+            768 : {
+                items:2
+            },
+            0 : {
+                items: 1
+            }
+        }
+    });
+    $(".logo-slider").owlCarousel({
+        loop:true,
+        dots:true,
+        dotsClass: "dots",
+        dotsContainer: ".dots-logo",
+        responsiveClass:true,
+        responsive:{
+            1200 : {
+                items:5
+            },
+            // breakpoint from 480 up
+            1000 : {
+                items:4
             },
             // breakpoint from 768 up
             768 : {
