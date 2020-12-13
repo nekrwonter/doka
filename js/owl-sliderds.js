a6a1e0e6beffdfc733e32d289c49dc478af311c8
Deus <nekrwonter@gmail.com>
2020-12-13 05:58:17 +0200

    TEXT-2 + VIDEOS

--- js/owl-sliderds.js
+++ js/owl-sliderds.js
@@ -66,4 +66,50 @@ $(document).ready(function(){
             }
         }
     });
+    $(".reviews-slider").owlCarousel({
+        loop:true,
+        dots:true,
+        dotsClass: "dots",
+        dotsContainer: ".dots-reviews",
+        responsiveClass:true,
+        responsive:{
+            1200 : {
+                items:3
+            },
+            // breakpoint from 480 up
+            1000 : {
+                items:2
+            },
+            // breakpoint from 768 up
+            768 : {
+                items:1
+            },
+            0 : {
+                items: 1
+            }
+        }
+    });
+    $(".text-2-videos").owlCarousel({
+        loop:true,
+        dots:true,
+        dotsClass: "dots",
+        dotsContainer: ".dots-videos",
+        responsiveClass:true,
+        responsive:{
+            1200 : {
+                items:4
+            },
+            // breakpoint from 480 up
+            1000 : {
+                items:3
+            },
+            // breakpoint from 768 up
+            768 : {
+                items:2
+            },
+            0 : {
+                items: 1
+            }
+        }
+    });
 });
