0acf724f2c6e3ff3e005bd5488b058c3ff5c4d1a
Deus <nekrwonter@gmail.com>
2020-12-14 04:55:45 +0200

    Refactor + best solution start

---
+++ js/fieldsets.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+
+    var current_fs, next_fs, previous_fs; //fieldsets
+    var opacity;
+
+    $(".next").on('click', function(){
+        current_fs = $(this).parent();
+        current_fs.css({
+            'display': 'none'
+        })
+        next_fs = $(this).parent().next();
+
+
+//show the next fieldset
+        next_fs.show();
+//hide the current fieldset with style
+        current_fs.animate({opacity: 0}, {
+            step: function(now) {
+// for making fielset appear animation
+                opacity = 1 - now;
+
+                current_fs.css({
+                    'display': 'none',
+                    'position': 'relative'
+                });
+                next_fs.css({'opacity': opacity});
+            },
+            duration: 600
+        });
+    });
+
+    $(".previous").click(function(){
+
+        current_fs = $(this).parent();
+        current_fs.css({
+            'display': 'none'
+        })
+        previous_fs = $(this).parent().prev();
+
+
+//show the previous fieldset
+        previous_fs.show();
+
+//hide the current fieldset with style
+        current_fs.animate({opacity: 0}, {
+            step: function(now) {
+// for making fielset appear animation
+                opacity = 1 - now;
+
+                current_fs.css({
+                    'display': 'none',
+                    'position': 'relative'
+                });
+                previous_fs.css({'opacity': opacity});
+            },
+            duration: 600
+        });
+    });
+
+    $('.radio-group .radio').click(function(){
+        $(this).parent().find('.radio').removeClass('selected');
+        $(this).addClass('selected');
+    });
+
+    $(".submit").click(function(){
+        return false;
+    })
+
+});
