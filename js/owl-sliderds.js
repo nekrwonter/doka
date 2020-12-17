36500f49961642d273b9938b58467ecd1f40f57e
Deus <nekrwonter@gmail.com>
2020-12-17 11:48:36 +0200

    arenda 0.9v

--- js/owl-sliderds.js
+++ js/owl-sliderds.js
@@ -122,4 +122,54 @@ $(document).ready(function(){
             }
         }
     });
+    $(".slider-arenda-towars").owlCarousel({
+        loop:true,
+        dots:true,
+        margin:20,
+        stagePadding: 10,
+        dotsClass: "dots",
+        dotsContainer: ".dots-towars",
+        responsiveClass:true,
+        responsive:{
+            1200 : {
+                items:3
+            },
+            // breakpoint from 480 up
+            1000 : {
+                items:2
+            },
+            // breakpoint from 768 up
+            768 : {
+                items:1
+            },
+            0 : {
+                items: 1
+            }
+        }
+    });
+    $(".images-slider").owlCarousel({
+        loop:true,
+        dots:true,
+        margin:20,
+        stagePadding: 10,
+        dotsClass: "dots",
+        dotsContainer: ".dots-images",
+        responsiveClass:true,
+        responsive:{
+            1200 : {
+                items:4
+            },
+            // breakpoint from 480 up
+            1000 : {
+                items:3
+            },
+            // breakpoint from 768 up
+            768 : {
+                items:2
+            },
+            0 : {
+                items: 1
+            }
+        }
+    });
 });
